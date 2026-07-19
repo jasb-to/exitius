4e851c5bea3ebc7a67d445d6ccd527a03537fd88
jasb-to <jaspalbilkhu@gmail.com>
2026-07-19 14:59:52 +0100

    Refactor Navbar component structure and links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,94 +3,106 @@ import Link from "next/link";
 
 export default function Navbar() {
 
-return (
+  return (
 
-<nav className="
-flex
-items-center
-justify-between
-py-6
-">
-
-
-<Link
-href="/"
-className="
-text-2xl
-font-bold
-tracking-tight
-"
->
-Exitius
-</Link>
+    <nav className="
+      flex
+      items-center
+      justify-between
+      py-6
+    ">
 
 
-<div className="
-hidden
-md:flex
-gap-8
-text-sm
-text-gray-600
-">
-
-
-<Link href="/search">
-Search
-</Link>
-
-
-<Link href="/pricing">
-Pricing
-</Link>
-
-
-<Link href="/about">
-About
-</Link>
-
-
-</div>
+      <Link
+        href="/"
+        className="
+          text-2xl
+          font-bold
+        "
+      >
+        Exitius
+      </Link>
 
 
 
-<div className="
-flex
-gap-3
-">
+      <div className="
+        hidden
+        md:flex
+        gap-6
+        text-sm
+        text-gray-600
+      ">
+
+        <Link href="/search">
+          Search
+        </Link>
 
 
-<Link
-href="/login"
-className="
-px-4
-py-2
-text-sm
-"
->
-Login
-</Link>
+        <Link href="/dashboard">
+          Dashboard
+        </Link>
 
 
-<Link
-href="/register"
-className="
-rounded-lg
-bg-black
-px-5
-py-2
-text-sm
-text-white
-"
->
-Start Free
-</Link>
+        <Link href="/pricing">
+          Pricing
+        </Link>
 
 
-</div>
+        <Link href="/about">
+          About
+        </Link>
 
 
-</nav>
+        <Link href="/contact">
+          Contact
+        </Link>
 
-);
+
+      </div>
+
+
+
+
+      <div className="
+        flex
+        items-center
+        gap-3
+      ">
+
+
+        <Link
+          href="/login"
+          className="
+            px-4
+            py-2
+            text-sm
+          "
+        >
+          Login
+        </Link>
+
+
+
+        <Link
+          href="/register"
+          className="
+            rounded-lg
+            bg-black
+            px-5
+            py-2
+            text-sm
+            text-white
+          "
+        >
+          Start Free
+        </Link>
+
+
+      </div>
+
+
+    </nav>
+
+  );
 
 }
